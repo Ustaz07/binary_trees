b9f7302e35fd59363454597261dec61cfe5ec8ab
Ustaz07 <shamsutukor@gmail.com>
2024-04-15 23:58:22 -0700

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,54 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
- * @first: Pointer to the first node
- * @second: Pointer to the second node
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
  *
- * Return: Pointer to the lowest common ancestor node, or NULL if no common ancestor was found
+ * Return: If no common ancestors return NULL, else return common ancestor.
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
-	const binary_tree_t *temp_first = first;
-	const binary_tree_t *temp_second = second;
-	size_t depth_first = 0, depth_second = 0;
+	binary_tree_t *mom, *pop;
 
-	if (first == NULL || second == NULL)
+	if (!first || !second)
 		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-	while (temp_first->parent != NULL)
-	{
-		temp_first = temp_first->parent;
-		depth_first++;
-	}
-
-	while (temp_second->parent != NULL)
-	{
-		temp_second = temp_second->parent;
-		depth_second++;
-	}
-
-	temp_first = first;
-	temp_second = second;
-
-	while (depth_first > depth_second)
-	{
-		temp_first = temp_first->parent;
-		depth_first--;
-	}
-
-	while (depth_second > depth_first)
-	{
-		temp_second = temp_second->parent;
-		depth_second--;
-	}
-
-	while (temp_first != temp_second)
-	{
-		temp_first = temp_first->parent;
-		temp_second = temp_second->parent;
-	}
-
-	return ((binary_tree_t *)temp_first);
+	mom = first->parent, pop = second->parent;
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	else if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
 }
 
