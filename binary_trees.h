aba4b9b00343769260443ff60b04132ce1f9a191
Ustaz07 <shamsutukor@gmail.com>
2024-04-17 03:53:09 -0700

    101-binary_tree_levelorder.c

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,15 @@ struct binary_tree_s
 };
 typedef struct binary_tree_s binary_tree_t;
 
+
+typedef struct levelorder_queue_s
+{
+    const binary_tree_t *node;
+    struct levelorder_queue_s *next;
+} levelorder_queue_t;
+
+
+
 /* Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
