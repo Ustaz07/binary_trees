92c7ae70b0f37d0a1b5e0f715e84668391818867
Ustaz07 <shamsutukor@gmail.com>
2024-04-17 03:13:43 -0700

    101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,39 +1,3 @@
-#include "binary_trees.h"
-#include <stdlib.h>
-
-/**
- * struct queue_s - Structure for a queue node
- * @node: Pointer to a binary tree node
- * @next: Pointer to the next node in the queue
- */
-typedef struct queue_s
-{
-    const binary_tree_t *node;
-    struct queue_s *next;
-} queue_t;
-
-/**
- * enqueue - Enqueues a node in the queue
- * @rear: Pointer to the rear of the queue
- * @node: Pointer to the node to be enqueued
- * Return: Pointer to the newly enqueued node
- */
-queue_t *enqueue(queue_t *rear, const binary_tree_t *node)
-{
-    queue_t *new_node = malloc(sizeof(queue_t));
-    if (new_node == NULL)
-        return NULL;
-
-    new_node->node = node;
-    new_node->next = NULL;
-
-    if (rear == NULL)
-        return new_node;
-
-    rear->next = new_node;
-    return new_node;
-}
-
 /**
  * dequeue - Dequeues a node from the queue
  * @front: Pointer to the front of the queue
@@ -41,29 +5,17 @@ queue_t *enqueue(queue_t *rear, const binary_tree_t *node)
  */
 const binary_tree_t *dequeue(queue_t **front)
 {
+    const binary_tree_t *node;
+
     if (*front == NULL)
         return NULL;
 
-    const binary_tree_t *node = (*front)->node;
+    node = (*front)->node;
     queue_t *temp = *front;
     *front = (*front)->next;
     free(temp);
 
     return node;
-}
-
-/**
- * free_queue - Frees a queue
- * @front: Pointer to the front of the queue
- */
-void free_queue(queue_t *front)
-{
-    while (front != NULL)
-    {
-        queue_t *temp = front;
-        front = front->next;
-        free(temp);
-    }
 }
 
 /**
